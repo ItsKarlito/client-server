7da34e3e6504e7a03180ba406b70e2aebb84758e
Karl Noory <karlnoory@hotmail.com>
2020-04-07 02:41:35 -0400

    Ran "JavaScript Standard Style" utility

--- server.js
+++ server.js
@@ -1,57 +1,55 @@
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
 
-let boxCount = 0;
-const boxMaxItemCount = 1000;
-const serverGreeting = 'connected to server...';
+let boxCount = 0
+const boxMaxItemCount = 1000
+const serverGreeting = 'connected to server...'
 
-let sigCount = 0;
+let sigCount = 0
 
 // serve static files
-app.use(express.static(__dirname));
-app.use(express.static(__dirname + '/node_modules'));
+app.use(express.static(__dirname))
+app.use(express.static(__dirname + '/node_modules'))
 app.get('/', function (req, res, next) {
-    res.sendFile(__dirname + '/index.html');
-});
+  res.sendFile(__dirname + '/index.html')
+})
 
 // The io.on is listening for connections. When it receives one it will report to the console client connected....
 // The 'client.on('join') will wait for a message from the client for join. It will then log it to the console.
 io.on('connection', function (client) {
-    client.on('join', function (data) {
-        // log client greeting
-        console.log(data);
+  client.on('join', function (data) {
+    // log client greeting
+    console.log(data)
 
-        function pushToClients(data)
-        {
-            if (boxCount >= boxMaxItemCount) {
-                boxCount = 0;
-                client.emit('clearBox', 'box cleared')
-            }
-            client.emit('broad', data);
-            client.broadcast.emit('broad', data);
-            boxCount++;
-            console.log('Broadcasting: ' + data);
-        }
+    function pushToClients (data) {
+      if (boxCount >= boxMaxItemCount) {
+        boxCount = 0
+        client.emit('clearBox', 'box cleared')
+      }
+      client.emit('broad', data)
+      client.broadcast.emit('broad', data)
+      boxCount++
+      console.log('Broadcasting: ' + data)
+    }
 
-        // greet the new client
-        client.emit('broad', serverGreeting);
+    // greet the new client
+    client.emit('broad', serverGreeting)
 
-        client.on('add', function () {
-            sigCount++;
-            pushToClients(sigCount);
-        });
-        
-        // broadcast client messages to other clients
-        client.on('messages', function (data) {
-            if (data != '')
-            {
-                pushToClients(data);
-            }
-        });
-    });
-});
+    client.on('add', function () {
+      sigCount++
+      pushToClients(sigCount)
+    })
 
-server.listen(3000);
-console.log('http://192.168.2.207:3000');
+    // broadcast client messages to other clients
+    client.on('messages', function (data) {
+      if (data != '') {
+        pushToClients(data)
+      }
+    })
+  })
+})
+
+server.listen(3000)
+console.log('http://192.168.2.207:3000')
