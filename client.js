377b71220858b3a14c18706318af720e6f1e4365
Karl Noory <karlnoory@hotmail.com>
2020-04-17 02:20:47 -0400

    improved user interface, header added to html page (server updates header info when needed)

--- client.js
+++ client.js
@@ -1,5 +1,22 @@
 const socket = io.connect('http://192.168.2.219:3000')
+
 const box = document.getElementById('box')
+
+function start () {
+  socket.emit('start')
+}
+
+function stop () {
+  socket.emit('stop')
+}
+
+function formatTimestamp (timeStamp) {
+  if (timeStamp === '') return timeStamp
+  const dateTime = new Date(timeStamp)
+  const formatedDate = `${dateTime.getDate()}/${dateTime.getMonth() + 1}/${dateTime.getFullYear()}`
+  const formatedTime = dateTime.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false })
+  return String(formatedTime + ' - ' + formatedDate)
+}
 
 socket.on('connect', function () {
   socket.emit('join')
@@ -16,10 +33,13 @@ socket.on('clearBox', function () {
   box.innerHTML = ''
 })
 
-function start() {
-  socket.emit('start')
-}
-
-function stop() {
-  socket.emit('stop')
-}
+socket.on('updateDatabaseHeader', function (data) {
+  document.getElementById('line').value = data.fillingLine
+  document.getElementById('product').value = data.product
+  document.getElementById('start').value = formatTimestamp(data.startTimestamp)
+  document.getElementById('end').value = formatTimestamp(data.endTimestamp)
+  document.getElementById('count').value = data.count
+  document.getElementById('totalTime').value = data.timeElapsed
+  document.getElementById('average').value = data.averageUnitPerUnitTime
+  document.getElementById('bracketSizeRunningAverage').value = data.bracketSizeRunningAverage
+})
--- server.js
+++ server.js
@@ -10,17 +10,41 @@ const button = new Gpio(17, 'in', 'falling', { debounceTimeout: 50 })
 
 let isRecording = false
 
-let signalCount = 0
 const signalCountFile = 'bottle_count.csv'
 const databaseFile = 'database.csv'
 
+const perUnitTime = 60000
+const bracket = []
+
+let boxCount = 0
+const boxMaxItemCount = 1000
+const serverGreeting = 'connected to server...'
+
+const databaseHeader = {
+  fillingLine: '',
+  product: '',
+  startTimestamp: '',
+  endTimestamp: '',
+  count: '',
+  timeElapsed: '',
+  averageUnitPerUnitTime: '',
+  bracketSizeRunningAverage: 5
+}
+
+app.use(express.static(__dirname))
+app.use(express.static(path.join(__dirname, '/database.csv')))
+app.use(express.static(path.join(__dirname, '/node_modules')))
+app.get('/', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '/index.html'))
+})
+
 fs.access(signalCountFile, (err) => {
   if (err) {
-    fs.appendFile(signalCountFile, signalCount, function (err) {
+    fs.appendFile(signalCountFile, databaseHeader.count, function (err) {
       if (err) throw err
     })
   } else {
-    signalCount = fs.readFileSync(signalCountFile)
+    databaseHeader.count = fs.readFileSync(signalCountFile)
   }
 })
 
@@ -32,60 +56,46 @@ fs.access(databaseFile, (err) => {
   }
 })
 
-const perUnitTime = 60000
-const bracketSize = 6
-const bracket = []
+function updateDatabaseHeader () {
+  io.emit('updateDatabaseHeader', databaseHeader)
+}
 
-let boxCount = 0
-const boxMaxItemCount = 1000
-const serverGreeting = 'connected to server...'
-
-app.use(express.static(__dirname))
-app.use(express.static(path.join(__dirname, '/database.csv')))
-app.use(express.static(path.join(__dirname, '/node_modules')))
-app.get('/', function (req, res, next) {
-  res.sendFile(path.join(__dirname, '/index.html'))
-})
-
-function writeToDatabase(data) {
+function writeToDatabase (data) {
   fs.appendFile(databaseFile, data + '\n', function (err) {
     if (err) throw err
   })
 }
 
-function incrementSignalCount() {
-  signalCount++
-  fs.writeFile(signalCountFile, signalCount, function (err) {
+function incrementSignalCount () {
+  databaseHeader.count++
+  fs.writeFile(signalCountFile, databaseHeader.count, function (err) {
     if (err) throw err
   })
 }
 
-function updateBracket(data) {
-  if (bracket.length >= bracketSize) {
+function updateBracket (timeStamp) {
+  if (bracket.length >= databaseHeader.bracketSizeRunningAverage + 1) {
     bracket.shift()
   }
-  bracket.push(data)
+  bracket.push(timeStamp)
 }
 
-function delta() {
-  const timeStamp1 = new Date(bracket[0])
-  const timeStamp2 = new Date(bracket[bracketSize - 1])
-  const time = (timeStamp2 - timeStamp1)
-  return time
+function deltaTimestamp (initial, final) {
+  return (new Date(final) - new Date(initial))
 }
 
-function average() {
-  return ((bracketSize - 1) * perUnitTime / delta())
+function runningAverage () {
+  return (databaseHeader.bracketSizeRunningAverage * perUnitTime / deltaTimestamp(bracket[0], bracket[databaseHeader.bracketSizeRunningAverage]))
 }
 
-function formatDateTime(data) {
-  const dateTime = new Date(data)
+function formatTimestamp (timeStamp) {
+  const dateTime = new Date(timeStamp)
   const formatedDate = `${dateTime.getDate()}/${dateTime.getMonth() + 1}/${dateTime.getFullYear()}`
   const formatedTime = dateTime.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false })
   return String('[ ' + formatedTime + ' - ' + formatedDate + ' ]')
 }
 
-function pushToClients(data) {
+function pushToClients (data) {
   if (boxCount >= boxMaxItemCount) {
     boxCount = 0
     io.emit('clearBox')
@@ -96,22 +106,28 @@ function pushToClients(data) {
 
 io.on('connection', function (client) {
   client.emit('broad', serverGreeting)
+  updateDatabaseHeader()
   client.on('start', function () {
     isRecording = true
+    databaseHeader.startTimestamp = new Date()
+    updateDatabaseHeader()
   })
   client.on('stop', function () {
     isRecording = false
+    databaseHeader.endTimestamp = new Date()
+    updateDatabaseHeader()
   })
 })
 
 button.watch((err) => {
+  if (err) throw err
   if (isRecording) {
-    if (err) throw err
-    incrementSignalCount()
     const timeStamp = new Date()
+    incrementSignalCount()
+    updateDatabaseHeader()
     updateBracket(timeStamp)
-    writeToDatabase(String(signalCount + ',' + Math.round(average()) + ',' + timeStamp.getHours() + ',' + timeStamp.getMinutes() + ',' + timeStamp.getSeconds() + ',' + timeStamp.getDate() + ',' + Number(timeStamp.getMonth() + 1) + ',' + timeStamp.getFullYear()) + ',' + timeStamp)
-    pushToClients('[ ' + signalCount + ' ]' + '[ ' + Math.round(average()) + ' ]' + formatDateTime(timeStamp))
+    writeToDatabase(String(databaseHeader.count + ',' + Math.round(runningAverage()) + ',' + timeStamp.getHours() + ',' + timeStamp.getMinutes() + ',' + timeStamp.getSeconds() + ',' + timeStamp.getDate() + ',' + Number(timeStamp.getMonth() + 1) + ',' + timeStamp.getFullYear()) + ',' + timeStamp)
+    pushToClients('[ ' + databaseHeader.count + ' ]' + '[ ' + Math.round(runningAverage()) + ' ]' + formatTimestamp(timeStamp))
   }
 })
 
